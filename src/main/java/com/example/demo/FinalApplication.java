b088f7805222a9bd05de813f2ddd1e8360e5cc0f
Doloto <wandbkk@gmail.com>
2025-12-19 22:22:27 +0500

    Removed unused imports from main class

--- src/main/java/com/example/demo/FinalApplication.java
+++ src/main/java/com/example/demo/FinalApplication.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
 
 @SpringBootApplication
 public class FinalApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FinalApplication.class, args);
 	}
 
 }
+
